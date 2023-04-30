3e0783d969624e809b8d414586390837ecb39245
JustMavist <ogohyh22@gmail.com>
2023-04-30 21:28:53 +0300

    mavist/менсон в стрингах

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,33 @@
+import java.util.Random;
+
 public class Main {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("mavmavmav");
+        int chislo = 12;
+        chislo = 11;
+        System.out.println("Твое очко: " + chislo + "см");
+
+         */
+        // byte - -128 - 127 / 1 байт
+        // short - -32768 - 32767 / 2 байт
+        // int - -2147783648 - 2147783647 / 4 байт
+        //long - -9 223 372 036 854 775 808 - 9 223 372 036 854 775 807 / 8 байт
+
+        float num1 = 12312123.01452f;
+        double num2 = 13123.1312322d;
+        double num3 = 11.1;
+
+        char c1 = 'm';
+
+        int chislo = 12;
+        int chislo2 = 1;
+        String stringi_igorya = "Есть";
+        boolean have_stringi = true;
+        Random rd = new Random();
+        System.out.println("Менсон в стрингах?" + rd.nextBoolean());
+
+    }
 
 }
