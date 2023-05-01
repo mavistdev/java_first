ee90867a9c0f72265077a114f001b42e73dc9634
JustMavist <ogohyh22@gmail.com>
2023-05-01 11:27:11 +0300

    mavist/Циклы

---
+++ src/cycles.java
@@ -0,0 +1,12 @@
+public class cycles {
+    public static void main(String[] args){
+        //for(float i = 10000; i > 10; i /= 2){
+        //   System.out.println(i);
+        //}
+        int x = 10000000;
+        while (x < 10){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
